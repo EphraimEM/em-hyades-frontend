7281332a4f27aa2049db3005bfff4576a61f4789
Ephraim Mensah <ephraim.e.mensah@lmco.com>
2025-02-06 23:22:58 +0000

    remove the gitlab integration url, group name and api input field
    Signed-off-by: Ephraim Mensah <ephraim.e.mensah@lmco.com>

--- src/views/administration/integrations/GitlabIntegration.vue
+++ src/views/administration/integrations/GitlabIntegration.vue
@@ -8,35 +8,6 @@
         label
         v-bind="labelIcon"
       />{{ $t('admin.integration_gitlab_enable') }}
-      <b-validated-input-group-form-input
-        id="Gitlab-Host"
-        :label="$t('admin.gitlab_host')"
-        input-group-size="mb-3"
-        rules="required"
-        type="text"
-        v-model="host"
-        lazy="true"
-      />
-
-      <b-validated-input-group-form-input
-        id="Group-Name"
-        :label="$t('Group Name')"
-        input-group-size="mb-3"
-        rules="required"
-        type="text"
-        v-model="groupName"
-        lazy="true"
-      />
-
-      <b-validated-input-group-form-input
-        id="Gitlab-Integration-apiKey"
-        :label="$t('GITLAB API TOKEN')"
-        input-group-size="mb-3"
-        rules="required"
-        type="password"
-        v-model="apiKey"
-        lazy="true"
-      />
     </b-card-body>
     <b-card-footer>
       <b-button variant="outline-primary" class="px-4" @click="saveChanges">{{
@@ -48,7 +19,6 @@
 
 <script>
 import { Switch as cSwitch } from '@coreui/vue';
-import BValidatedInputGroupFormInput from '../../../forms/BValidatedInputGroupFormInput';
 // eslint-disable-next-line no-unused-vars
 import axios from 'axios'; // Import axios
 import common from '../../../shared/common';
@@ -61,14 +31,10 @@ export default {
   },
   components: {
     cSwitch,
-    BValidatedInputGroupFormInput,
   },
   data() {
     return {
       enabled: false,
-      host: '',
-      url: '',
-      apiKey: '',
     };
   },
   methods: {
@@ -79,21 +45,6 @@ export default {
             groupName: 'integrations',
             propertyName: 'gitlab_integration.enabled',
             propertyValue: this.enabled,
-          },
-          {
-            groupName: 'integrations',
-            propertyName: 'gitlab_integration.host',
-            propertyValue: this.host,
-          },
-          {
-            groupName: 'integrations',
-            propertyName: 'gitlab_integration.url',
-            propertyValue: this.url,
-          },
-          {
-            groupName: 'integrations',
-            propertyName: 'gitlab_integration.apiKey',
-            propertyValue: this.apiKey,
           },
         ]);
       } catch (error) {
@@ -113,15 +64,6 @@ export default {
           switch (item.propertyName) {
             case 'gitlab_integration.enabled':
               this.enabled = common.toBoolean(item.propertyValue);
-              break;
-            case 'gitlab_integration.host':
-              this.host = item.propertyValue;
-              break;
-            case 'gitlab_integration.url':
-              this.url = item.propertyValue;
-              break;
-            case 'gitlab_integration.apiKey':
-              this.apiKey = item.propertyValue;
               break;
           }
         }
